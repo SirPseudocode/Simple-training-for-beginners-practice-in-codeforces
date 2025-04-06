8680ea842e331f13954149da58bb2dd4a91740d9
SirPseudocode <dafrad.tya@gmail.com>
2025-04-06 15:56:45 +0800

    Create 100 Problems (A) folder and add Team.cpp to the folder

---
+++ 100 Problems (A)/Team.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+
+    int amount = 0,x,y,z;
+    while(n--){
+        cin >> x >> y >> z;
+        if(x + y + z >= 2){
+            amount++;
+        }
+    }
+    cout << amount;
+    return 0;
+}
